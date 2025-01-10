0afaa9d35ed4041ec8335d36a2581b717dcb0d10
AnaMarijaBakula <ana.marija.bakula@fpmoz.sum.ba>
2025-01-10 10:22:23 +0100

    Components:HomeScreen & AddNew

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeScreen from '../components/HomeScreen.vue'
+import AddNew   from "@/components/AddNew.vue";
+</script>
+
+<template>
+  <main>
+    <HomeScreen />
+  </main>
+</template>
